fb5c4c4321e91e24bee09b0b4310504bcabe1304
dhanananjai <dhananjaikrpandey@gmail.com>
2023-05-07 12:27:53 +0530

    random commit

---
+++ src/main/java/Pages/SearchPage.java
@@ -0,0 +1,27 @@
+package Pages;
+
+import DriverUtil.DriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class SearchPage {
+
+    WebDriver driver = DriverManager.getDriver();
+    By productTitles = By.xpath("//h2[@class = \"product-title\"]/a");
+
+    public int getProductTitles(String item{
+        List <WebElement> titles = driver.findElements(productTitles);
+        int count=0;
+        for (WebElement element:titles) {
+            if (element.getText().toLowerCase().contains(item)){
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+}
